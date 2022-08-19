f8f68fe0108956b2d012051a093e9c621ba0f068
Edward Shao <edwardxzshao@gmail.com>
2022-08-18 22:16:20 -0400

    created main.py, updated README, updated gitignore

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PCDealsBot
 Discord bot sending the hottest deals on PC parts (NA only)
+
+
+# How to work on development (for Mark)
+
+# Install dependencies:
+pip install -U discord.py
+pip install python-dotenv
---
+++ main.py
@@ -0,0 +1,27 @@
+# Imports
+import discord
+import os
+from dotenv import load_dotenv
+
+# Credentials
+load_dotenv('.env')
+
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# ???
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user: 
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
